4a405f5674602095c10dd6b5675a710af44d6281
Matheus Schmitt <matheusschmittmr45@gmail.com>
2024-06-02 19:38:20 -0300

    Update --> Login Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/tarefa/Login";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ListarTarefa />
+      <Login />
     </div>
   );
 }
